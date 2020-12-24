c449b21ad5d5618924ca1417cc73cb1a8c2a97e1
MahmudMahi <76428500+MahmudMahi@users.noreply.github.com>
2020-12-24 12:44:00 +0600

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,9 +1,19 @@
-var name = prompt("what is your name , please?");
-for (var i = 0; i < 8; i++) {
-     j = i.toString();
-     var element = document.getElementById('name'+j);
-     element.innerHTML=name;
- }
+var name = prompt("what is your NickName , please?" , "Your Nick Name");
+if (name==="Your Nick Name") {
+    alert("ALERT! You haven't set your nickname , So you'll be adressed as Random person the whole time. :( ")
+    for (var i = 0; i < 8; i++) {
+        j = i.toString();
+        var element = document.getElementById('name'+j);
+        element.innerHTML = "Random Person";
+    }
+}
+else{
+  for (var i = 0; i < 8; i++) {
+    j = i.toString();
+    var element = document.getElementById('name'+j);
+    element.innerHTML = name;
+    }
+}
 
 
 
