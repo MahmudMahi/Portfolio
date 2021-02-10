4a717ecb50c6d654614ba34995f0edd21753ba3c
Mahmud <76428500+MahmudMahi@users.noreply.github.com>
2021-02-10 12:03:26 +0600

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,21 +1,4 @@
-
-
-var name = prompt("what is your NickName , please?" , "Your Nick Name");
-if (name==="Your Nick Name") {
-    alert("ALERT! You haven't set your nickname , So you'll be adressed as Random person the whole time. :( ")
-    for (var i = 0; i < 8; i++) {
-        j = i.toString();
-        var element = document.getElementById('name'+j);
-        element.innerHTML = "Random Person";
-    }
-}
-else{
-  for (var i = 0; i < 8; i++) {
-    j = i.toString();
-    var element = document.getElementById('name'+j);
-    element.innerHTML = name;
-    }
-}
+alert(" MESSAGE: This Site is Still Under Construction");
 
 
 
