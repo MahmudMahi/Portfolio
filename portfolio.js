d068bf8b7a176e62fef4cee92575a9b954852ea4
Mahmud <76428500+MahmudMahi@users.noreply.github.com>
2021-02-12 10:36:52 +0600

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,11 @@
-alert(" MESSAGE: This Site is Still Under Construction");
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
 
 
 
